0ef3f4582f8ec6820d5453f2d529d5a64f142a10
unknown <yaradayoub2019@gmail.com>
2025-07-18 18:31:44 +0300

    kk

--- helpers/constants.js
+++ helpers/constants.js
@@ -13,9 +13,6 @@ export const SELECTORS = {
     // This is typically the original price, not the wholesale cost.
     // I'm naming it ORIGINAL_OR_STRIKE_PRICE to reflect its likely purpose on a public site.
     ORIGINAL_OR_STRIKE_PRICE: '.body-regular.price-strike',
-    // You might still have a current/sale price selector if you want to extract it separately
-    CURRENT_PRICE: '.price-info .current-price, .price-info .sale-price', // Example for current/sale price
-
     // Description & Features
     DESCRIPTION_BUTTON: 'button.switch.link-med',
     DESCRIPTION_CONTENT_CONTAINER: 'div#details-drawer',
@@ -29,18 +26,18 @@ export const SELECTORS = {
     // are likely for *displaying* the currently selected variant, not for *clicking* to select variants.
     // I will use more common selectors for *clickable elements* for iteration,
     // and note the ones you provided if they are for getting the *value* of the selected item.
-    COLOR_OPTION_NAME: 'span.updated-label.label', // This appears to be the generic label for the option name
+    COLOR_OPTION_NAME: 'img.color-swatch-sprite', // This appears to be the generic label for the option name
     // For clickable color swatches, usually a label or div wrapping a radio input
-    COLOR_RADIO_LABELS: '.color-swatches .color-swatch-item label, .color-swatches [data-auto="color-swatch-label"]', // More general selectors for clickable elements.
+    COLOR_RADIO_LABELS: 'span[data-testid="selected-color-name"]', // More general selectors for clickable elements.
     // The user provided `span data-testid="selected-color-name"` which is likely the display of the *selected* color value.
     SELECTED_COLOR_VALUE_DISPLAY: 'span[data-testid="selected-color-name"]', // For extracting the value of the currently selected color
 
-    SIZE_OPTION_NAME: 'span.updated-label.label', // This appears to be the generic label for the option name
+    SIZE_OPTION_NAME: 'label.size-tile .selection-tile', // This appears to be the generic label for the option name
     // For clickable size chips, usually a label or div wrapping a radio input
     SIZE_RADIO_LABELS: '.size-selector .size-chip-item label, .size-selector [data-auto="size-tile-label"]', // More general selectors for clickable elements.
     // The user provided `span id="selection-tile-5036768-1"` which is too specific.
     // This is likely the display of the *selected* size value.
-    SELECTED_SIZE_VALUE_DISPLAY: '.size-selector span[id^="selection-tile-"]', // For extracting the value of the currently selected size (starts with selection-tile-)
+    SELECTED_SIZE_VALUE_DISPLAY: 'span.updated-label .label > span', // For extracting the value of the currently selected size (starts with selection-tile-)
   },
   BREADCRUMBS: {
     LINKS: 'ol.p-breadcrumb-list > li.p-menuitem > a',
